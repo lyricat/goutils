aa22b63ad47868d71125297c211e1f4542e95a63
Lyric <5h3ll3x@gmail.com>
2024-09-29 09:39:15 +0900

    feat: add qdrant

--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/ai/azopenai v0.6.0
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.14.0
 	github.com/sashabaranov/go-openai v1.28.1
+	github.com/shopspring/decimal v1.3.1
+	golang.org/x/exp v0.0.0-20221106115401-f9659909a136
+	google.golang.org/grpc v1.67.0
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
@@ -21,11 +24,10 @@ require (
 	github.com/lestrrat-go/iter v1.0.2 // indirect
 	github.com/lestrrat-go/option v1.0.1 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
-	github.com/shopspring/decimal v1.3.1 // indirect
-	golang.org/x/crypto v0.25.0 // indirect
-	golang.org/x/exp v0.0.0-20221106115401-f9659909a136 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/sys v0.24.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240827150818-7e3bb234dfed // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 )
 
 require (
@@ -37,9 +39,10 @@ require (
 	github.com/lestrrat-go/jwx/v2 v2.1.1
 	github.com/line/line-bot-sdk-go/v8 v8.7.0
 	github.com/pemistahl/lingua-go v1.4.0
+	github.com/qdrant/go-client v1.11.1
 	github.com/redis/go-redis/v9 v9.6.1
-	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/oauth2 v0.22.0
-	golang.org/x/text v0.16.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 	gopkg.in/square/go-jose.v2 v2.6.0
 )
---
+++ qdrant/qdrant.go
@@ -0,0 +1,378 @@
+package qdrant
+
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"log/slog"
+	"time"
+
+	pb "github.com/qdrant/go-client/qdrant"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+)
+
+type (
+	QdrantPoint struct {
+		ID      int64            `json:"id"`
+		Score   float32          `json:"score"`
+		Vector  []float32        `json:"vector"`
+		Payload map[string]int64 `json:"payload"`
+	}
+
+	Config struct {
+		Addr   string
+		APIKey string
+	}
+
+	QdrantClient struct {
+		Addr   string
+		APIKey string
+		ColCli pb.CollectionsClient
+		Conn   *grpc.ClientConn
+	}
+
+	CommonParams struct {
+		CollectionName string
+		PointID        uint64
+		PointUUID      string
+	}
+
+	GetPointsParams struct {
+		CommonParams
+	}
+
+	UpsertPointsParams struct {
+		CommonParams
+		Vector     []float32
+		Payload    map[string]UpsertPointPayloadItem
+		WaitUpsert bool
+	}
+
+	UpsertPointPayloadItem struct {
+		Type  string
+		Value any
+	}
+
+	DeletePointsParams struct {
+		CommonParams
+	}
+
+	SearchPointsParams struct {
+		CollectionName string
+		Vector         []float32
+		TopK           uint64
+		Key            string
+		Value          int64
+		Offset         uint64
+	}
+
+	CreateCollectionParams struct {
+		CollectionName string
+		VectorSize     uint64
+		Indexes        []CreateCollectionIndexItem
+	}
+
+	CreateCollectionIndexItem struct {
+		Name string
+		Type string
+	}
+)
+
+func (p *CommonParams) GetPbPointIDs() ([]*pb.PointId, error) {
+	ids := make([]*pb.PointId, 0)
+	if p.PointID > 0 {
+		ids = append(ids, &pb.PointId{PointIdOptions: &pb.PointId_Num{Num: p.PointID}})
+	} else if p.PointUUID != "" {
+		ids = append(ids, &pb.PointId{PointIdOptions: &pb.PointId_Uuid{Uuid: p.PointUUID}})
+	} else {
+		slog.Error("point ID or UUID is required")
+		return nil, fmt.Errorf("point ID or UUID is required")
+	}
+	return ids, nil
+}
+
+func (p *CommonParams) GetPbPointID() (*pb.PointId, error) {
+	var id *pb.PointId
+	if p.PointID > 0 {
+		id = &pb.PointId{PointIdOptions: &pb.PointId_Num{Num: p.PointID}}
+	} else if p.PointUUID != "" {
+		id = &pb.PointId{PointIdOptions: &pb.PointId_Uuid{Uuid: p.PointUUID}}
+	} else {
+		slog.Error("point ID or UUID is required")
+		return nil, fmt.Errorf("point ID or UUID is required")
+	}
+	return id, nil
+}
+
+func genInterceptor(apiKey string) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		newCtx := metadata.AppendToOutgoingContext(ctx, "api-key", apiKey)
+		return invoker(newCtx, method, req, reply, cc, opts...)
+	}
+}
+
+func New(cfg Config) *QdrantClient {
+	config := &tls.Config{}
+	interceptor := genInterceptor(cfg.APIKey)
+	conn, err := grpc.NewClient(cfg.Addr, grpc.WithTransportCredentials(credentials.NewTLS(config)), grpc.WithUnaryInterceptor(interceptor))
+	if err != nil {
+		slog.Error("did not connect", "error", err)
+		panic(err)
+	}
+
+	ColCli := pb.NewCollectionsClient(conn)
+
+	return &QdrantClient{
+		Addr:   cfg.Addr,
+		APIKey: cfg.APIKey,
+		ColCli: ColCli,
+		Conn:   conn,
+	}
+}
+
+func (c *QdrantClient) Check() (string, error) {
+	qdrantClient := pb.NewQdrantClient(c.Conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	healthCheckResult, err := qdrantClient.HealthCheck(ctx, &pb.HealthCheckRequest{})
+	if err != nil {
+		slog.Error("could not get health", "error", err)
+		return "", err
+	}
+	return healthCheckResult.GetVersion(), nil
+}
+
+func (c *QdrantClient) Close() {
+	c.Conn.Close()
+}
+
+func (c *QdrantClient) GetPoints(ctx context.Context, params GetPointsParams) (*QdrantPoint, error) {
+	ids, err := params.GetPbPointIDs()
+	if err != nil {
+		return nil, err
+	}
+
+	pointsClient := pb.NewPointsClient(c.Conn)
+	pointsById, err := pointsClient.Get(ctx, &pb.GetPoints{
+		CollectionName: params.CollectionName,
+		Ids:            ids,
+		WithVectors:    &pb.WithVectorsSelector{SelectorOptions: &pb.WithVectorsSelector_Enable{Enable: true}},
+		WithPayload:    &pb.WithPayloadSelector{SelectorOptions: &pb.WithPayloadSelector_Enable{Enable: true}},
+	})
+	if err != nil {
+		slog.Error("could not retrieve points", "error", err)
+		return nil, err
+	}
+	result := pointsById.GetResult()
+	if len(result) > 0 {
+		qp := &QdrantPoint{}
+		qp.LoadFromRetrievedPoint(result[0])
+		return qp, nil
+	}
+	return nil, nil
+}
+
+func (c *QdrantClient) UpsertPoints(ctx context.Context, params UpsertPointsParams) error {
+	payload := make(map[string]*pb.Value)
+	for k, v := range params.Payload {
+		switch v.Type {
+		case "int":
+			payload[k] = &pb.Value{
+				Kind: &pb.Value_IntegerValue{IntegerValue: v.Value.(int64)},
+			}
+		case "double":
+			payload[k] = &pb.Value{
+				Kind: &pb.Value_DoubleValue{DoubleValue: v.Value.(float64)},
+			}
+		case "bool":
+			payload[k] = &pb.Value{
+				Kind: &pb.Value_BoolValue{BoolValue: v.Value.(bool)},
+			}
+		case "text":
+			payload[k] = &pb.Value{
+				Kind: &pb.Value_StringValue{StringValue: v.Value.(string)},
+			}
+		}
+	}
+
+	waitUpsert := params.WaitUpsert
+
+	id, err := params.GetPbPointID()
+	if err != nil {
+		return err
+	}
+
+	upsertPoints := []*pb.PointStruct{
+		{
+			Id:      id,
+			Vectors: &pb.Vectors{VectorsOptions: &pb.Vectors_Vector{Vector: &pb.Vector{Data: params.Vector}}},
+			Payload: payload,
+		},
+	}
+
+	pointsClient := pb.NewPointsClient(c.Conn)
+
+	if _, err := pointsClient.Upsert(ctx, &pb.UpsertPoints{
+		CollectionName: params.CollectionName,
+		Wait:           &waitUpsert,
+		Points:         upsertPoints,
+	}); err != nil {
+		slog.Error("could not upsert points", "error", err)
+	}
+
+	return nil
+}
+
+func (c *QdrantClient) DeletePoints(ctx context.Context, params DeletePointsParams) error {
+	ids, err := params.GetPbPointIDs()
+	if err != nil {
+		return err
+	}
+
+	pointsClient := pb.NewPointsClient(c.Conn)
+	if _, err := pointsClient.Delete(ctx, &pb.DeletePoints{
+		CollectionName: params.CollectionName,
+		Points: &pb.PointsSelector{
+			PointsSelectorOneOf: &pb.PointsSelector_Points{
+				Points: &pb.PointsIdsList{
+					Ids: ids,
+				},
+			},
+		},
+	}); err != nil {
+		slog.Error("could not delete points", "error", err)
+	}
+
+	return nil
+}
+
+func (c *QdrantClient) SearchPointsWithFilter(ctx context.Context, params SearchPointsParams) ([]*QdrantPoint, error) {
+	filter := &pb.Filter{}
+	if params.Key != "" {
+		filter = &pb.Filter{
+			Must: []*pb.Condition{
+				{
+					ConditionOneOf: &pb.Condition_Field{
+						Field: &pb.FieldCondition{
+							Key: params.Key,
+							Match: &pb.Match{
+								MatchValue: &pb.Match_Integer{
+									Integer: params.Value,
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+	pointsClient := pb.NewPointsClient(c.Conn)
+	filteredSearchResult, err := pointsClient.Search(ctx, &pb.SearchPoints{
+		CollectionName: params.CollectionName,
+		Vector:         params.Vector,
+		Limit:          params.TopK,
+		Offset:         &params.Offset,
+		Filter:         filter,
+		WithPayload:    &pb.WithPayloadSelector{SelectorOptions: &pb.WithPayloadSelector_Enable{Enable: true}},
+	})
+	if err != nil {
+		slog.Error("Could not search points", "error", err)
+	}
+
+	result := filteredSearchResult.GetResult()
+	qpList := make([]*QdrantPoint, 0)
+	for _, p := range result {
+		qp := &QdrantPoint{}
+		qp.LoadFromScoredPoint(p)
+		qpList = append(qpList, qp)
+	}
+	return qpList, nil
+}
+
+func (c *QdrantClient) CreateCollections(ctx context.Context, params CreateCollectionParams) error {
+	// Create new collection
+	var defaultSegmentNumber uint64 = 2
+	cols := []string{params.CollectionName}
+	for _, collectionName := range cols {
+		_, err := c.ColCli.Create(ctx, &pb.CreateCollection{
+			CollectionName: collectionName,
+			VectorsConfig: &pb.VectorsConfig{Config: &pb.VectorsConfig_Params{
+				Params: &pb.VectorParams{
+					Size:     params.VectorSize,
+					Distance: pb.Distance_Dot,
+				},
+			}},
+			OptimizersConfig: &pb.OptimizersConfigDiff{
+				DefaultSegmentNumber: &defaultSegmentNumber,
+			},
+		})
+		if err != nil {
+			slog.Error("could not create collection", "error", err)
+			return err
+		}
+		// Create string field index
+		pointsClient := pb.NewPointsClient(c.Conn)
+		for _, indexItem := range params.Indexes {
+			fieldType := pb.FieldType_FieldTypeKeyword
+			switch indexItem.Type {
+			case "int":
+				fieldType = pb.FieldType_FieldTypeInteger
+			case "float":
+				fieldType = pb.FieldType_FieldTypeFloat
+			case "bool":
+				fieldType = pb.FieldType_FieldTypeBool
+			case "text":
+				fieldType = pb.FieldType_FieldTypeText
+			case "keyword":
+				fieldType = pb.FieldType_FieldTypeKeyword
+			case "date":
+				fieldType = pb.FieldType_FieldTypeDatetime
+			case "geo":
+				fieldType = pb.FieldType_FieldTypeGeo
+			case "uuid":
+				fieldType = pb.FieldType_FieldTypeUuid
+			}
+
+			_, err := pointsClient.CreateFieldIndex(ctx, &pb.CreateFieldIndexCollection{
+				CollectionName: collectionName,
+				FieldName:      indexItem.Name,
+				FieldType:      &fieldType,
+			})
+
+			if err != nil {
+				slog.Error("could not create index", "field", indexItem.Name, "error", err)
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (qp *QdrantPoint) LoadFromRetrievedPoint(p *pb.RetrievedPoint) error {
+	qp.ID = int64(p.Id.GetNum())
+	qp.Score = 0
+	qp.Vector = p.GetVectors().GetVector().GetData()
+	payload := p.GetPayload()
+	qp.Payload = make(map[string]int64)
+	for k, v := range payload {
+		qp.Payload[k] = v.GetIntegerValue()
+	}
+	return nil
+}
+
+func (qp *QdrantPoint) LoadFromScoredPoint(p *pb.ScoredPoint) error {
+	qp.ID = int64(p.Id.GetNum())
+	qp.Score = p.Score
+	qp.Vector = p.GetVectors().GetVector().GetData()
+	payload := p.GetPayload()
+	qp.Payload = make(map[string]int64)
+	for k, v := range payload {
+		qp.Payload[k] = v.GetIntegerValue()
+	}
+	return nil
+}
